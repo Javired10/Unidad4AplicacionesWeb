f6113c636cd7725966b9b6c23d054990508f275e
javired10 <pmarcac@est.ups.edu.ec>
2026-05-31 00:23:57 -0500

    solucion a url de netlify

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    // Usa la variable de entorno de Vite o cae en localhost si no está definida
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
     headers: {
         'Content-Type': 'application/json'
     }
